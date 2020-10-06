1eee33835ecde0bba668c05d8d5b96a810ef9338
phoenix <albphoenix@gmail.com>
2020-10-06 14:51:58 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,51 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+function getConfig(bracket, config) {
+    for (let brackets of config) {
+        if (brackets.includes(bracket)) {
+            return brackets;
+        }
+    }
+    return null;
 }
+
+module.exports = function check(str, bracketsConfig) {
+    let openedBrackets = [];
+
+    for (let bracket of str) {
+        let [openingBracket, closingBracket] = getConfig(bracket, bracketsConfig);
+        let prevOpenedBracket = openedBrackets[openedBrackets.length - 1];
+
+        if (openingBracket === closingBracket) {
+            if (!openedBrackets.includes(bracket)) {
+                openedBrackets.push(bracket);
+                continue;
+            }
+
+            if (openedBrackets.includes(bracket) && prevOpenedBracket === bracket) {
+                openedBrackets.pop();
+                continue;
+            }
+
+            if (prevOpenedBracket !== bracket) {
+                return false;
+            }
+        }
+
+        if (closingBracket === bracket) {
+            if (openedBrackets.length === 0) {
+                return false;
+            }
+
+            if (prevOpenedBracket === openingBracket) {
+                openedBrackets.pop();
+            } else {
+                return false;
+            }
+        }
+
+        if (openingBracket === bracket) {
+            openedBrackets.push(bracket);
+        }
+    }
+
+    return openedBrackets.length === 0;
+};
